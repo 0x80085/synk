8fb127eb7a40ef63f59629c17ec6ecaa45496390
barb3ra <lar.slugs@gmail.com>
2019-10-25 20:08:27 +0200

    play after init +  cleanup

--- synk-client/src/app/pages/channel/media/youtube/youtube.component.ts
+++ synk-client/src/app/pages/channel/media/youtube/youtube.component.ts
@@ -7,43 +7,34 @@ import { BaseMediaComponent } from '../base-media.component';
   styleUrls: ['./youtube.component.scss']
 })
 export class YoutubeComponent implements BaseMediaComponent, OnInit {
-
   @Input() url: string;
+
+  isReady = false;
 
   player: YT.Player;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-
     if (!(window as any).YT) {
-      // 2. This code loads the IFrame Player API code asynchronously.
+      console.log('Building player...');
+
       const tag = document.createElement('script');
       tag.src = 'http://www.youtube.com/iframe_api';
 
       const firstScriptTag = document.getElementsByTagName('script')[0];
       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-      // 3. This function creates an <iframe> (and YouTube player)
-      //    after the API code downloads.
-      (window as any).onYouTubeIframeAPIReady = (() => {
-        this.createPlayer()
-      });
+      (window as any).onYouTubeIframeAPIReady = () => {
+        this.createPlayer();
+      };
     } else {
-
-      this.createPlayer()
-
+      this.createPlayer();
     }
-
-  }
-
-  start?(url: string) {
-    // this.player.cueVideoById(url);
-    // this.player.playVideo();
   }
 
   play(): void {
-    // this.player.playVideo();
+    this.player.playVideo();
   }
 
   pause(): void {
@@ -54,20 +45,14 @@ export class YoutubeComponent implements BaseMediaComponent, OnInit {
     this.player.seekTo(to, true);
   }
 
-  // 4. The API will call this function when the video player is ready.
   onPlayerReady(event) {
     this.player = event.target;
-    // this.player.cueVideoByUrl(this.url)
-
-    // this.start(this.url) 
+    this.play();
+    this.isReady = true;
   }
 
-  // 5. The API calls this function when the player's state changes.
-  //    The function indicates that when playing a video (state=1),
-  //    the player should play for six seconds and then stop.
   onPlayerStateChange(event) {
     console.log(event);
-
   }
 
   stopVideo() {
@@ -80,23 +65,23 @@ export class YoutubeComponent implements BaseMediaComponent, OnInit {
       width: '100%',
       videoId: YouTubeGetID(this.url),
       events: {
-        onReady: (ev) => this.onPlayerReady(ev),
-        onError: (ev) => console.log(ev),
-        onStateChange: (ev) => this.onPlayerStateChange(ev)
+        onReady: ev => this.onPlayerReady(ev),
+        onError: ev => console.log(ev),
+        onStateChange: ev => this.onPlayerStateChange(ev)
       }
     });
   }
-
 }
 
 function YouTubeGetID(url) {
-  var ID = '';
-  url = url.replace(/(>|<)/gi, '').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+  let ID = '';
+  url = url
+    .replace(/(>|<)/gi, '')
+    .split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
   if (url[2] !== undefined) {
     ID = url[2].split(/[^0-9a-z_\-]/i);
     ID = ID[0];
-  }
-  else {
+  } else {
     ID = url;
   }
   return ID;
