7d04040086c3564cf9a399b61d735f21c90e6fd6
barb3ra <lar.slugs@gmail.com>
2019-11-05 23:04:10 +0100

    Draft for syncing the player

--- synk-client/src/app/pages/channel/channel.component.ts
+++ synk-client/src/app/pages/channel/channel.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MediaComponent } from './media/media.component';
-import { ChatService } from './chat.service';
+import { ChatService, MediaEvent } from './chat.service';
 import { timer, Subscription } from 'rxjs';
 
 @Component({
@@ -16,7 +16,8 @@ export class ChannelComponent implements OnInit, OnDestroy {
   mediaUrl = 'https://www.youtube.com/watch?v=_Jw7_DLlU4s';
   isLeader?: boolean = true;
 
-  mediaUpdateTimer: Subscription;
+  mediaUpdateTimer$: Subscription;
+  mediaSyncEvent$: Subscription;
 
   constructor(
     private route: ActivatedRoute,
@@ -25,15 +26,24 @@ export class ChannelComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.name = this.route.snapshot.paramMap.get('name');
+    this.syncPlayerState();
     this.sendPeriodicUpdate();
   }
 
   sendPeriodicUpdate() {
     const source = timer(1000, 2000);
-    this.mediaUpdateTimer = source.subscribe(val => {
+    this.mediaUpdateTimer$ = source.subscribe(val => {
       if (this.isLeader) {
         console.log('sending update');
         this.sendMediaUpdate();
+      }
+    });
+  }
+
+  syncPlayerState() {
+    this.mediaSyncEvent$ = this.chatService.roomMediaEvent$.subscribe(ev => {
+      if (!this.isLeader) {
+        this.syncIfNeeded(ev);
       }
     });
   }
@@ -50,7 +60,49 @@ export class ChannelComponent implements OnInit, OnDestroy {
     }
   }
 
+  private syncIfNeeded(ev: MediaEvent) {
+    try {
+      if (this.shouldSyncPlayer(ev)) {
+        console.log('updating player');
+
+        this.syncPlayer(ev);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  private syncPlayer(ev: MediaEvent) {
+    if (this.mediaUrl !== ev.mediaUrl) {
+      this.mediaUrl = ev.mediaUrl;
+      this.player.play();
+    }
+    if (this.clientCurrenttimeIsOutOfSync(ev.currentTime)) {
+      this.player.seek(ev.currentTime);
+    }
+  }
+
+  private shouldSyncPlayer(ev: MediaEvent) {
+    return (
+      this.mediaUrl !== ev.mediaUrl ||
+      this.clientCurrenttimeIsOutOfSync(ev.currentTime)
+    );
+  }
+
+  private clientCurrenttimeIsOutOfSync(originTime: number) {
+    try {
+      const clientTime = this.player.getCurrentTime();
+      const maxTimeBehind = originTime - 2;
+      const maxTimeAhead = originTime + 2;
+
+      return clientTime < maxTimeAhead && clientTime > maxTimeBehind;
+    } catch (error) {
+      return false; // getCurrentTime failed prob
+    }
+  }
+
   ngOnDestroy(): void {
-    this.mediaUpdateTimer.unsubscribe();
+    this.mediaUpdateTimer$.unsubscribe();
+    this.mediaSyncEvent$.unsubscribe();
   }
 }
