ef28c588ffe72077895f2741e5ac7f05a6034b09
barb3ra <lar.slugs@gmail.com>
2019-12-14 23:33:39 +0100

    Remove dupe docker db setup

--- synk-db/init.sql
+++
@@ -1,9 +0,0 @@
-/*
-    https://stackoverflow.com/questions/26598738/how-to-create-user-database-in-script-for-docker-postgres
-*/
-
-CREATE USER 'synk-db-user'
-WITH PASSWORD 'synk-db-user-pass';
-
-CREATE DATABASE synk;
-GRANT ALL PRIVILEGES ON DATABASE synk TO 'synk-db-user';
