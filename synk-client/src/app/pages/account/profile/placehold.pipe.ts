62e9fdbecf6244ad381a5e22bec89ab10d52f53f
barb3ra <lar.slugs@gmail.com>
2019-12-22 19:57:14 +0100

    color the placeholder img

--- synk-client/src/app/pages/account/profile/placehold.pipe.ts
+++ synk-client/src/app/pages/account/profile/placehold.pipe.ts
@@ -8,7 +8,7 @@ export class PlaceholdPipe implements PipeTransform {
 
   transform(user: User): string | null {
 
-    return user ? `http://satyr.io/400x300?text=${user.userName}` : `http://satyr.io/105x60?text=uh+oh`;
+    return user ? `http://satyr.io/400x300/c0ffee?text=${user.userName}` : `http://satyr.io/105x60?text=uh+oh`;
   }
 
 }
