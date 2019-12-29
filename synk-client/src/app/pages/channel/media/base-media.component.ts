70725aba799d0e49ef4e86f56e4f75decfcf1228
barb3ra <lar.slugs@gmail.com>
2019-12-29 23:21:59 +0100

    remove start() and add curenturl

--- synk-client/src/app/pages/channel/media/base-media.component.ts
+++ synk-client/src/app/pages/channel/media/base-media.component.ts
@@ -3,7 +3,6 @@ import { Output, EventEmitter } from '@angular/core';
 export interface BaseMediaComponent {
   videoEnded: EventEmitter<unknown>;
   isPlaying(): boolean;
-  start?(url: string): void;
   play(url?: string): void;
   pause(): void;
   seek(to: number): void;
