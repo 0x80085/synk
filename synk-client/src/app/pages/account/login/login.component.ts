89c848d30d612f29fdb6c549efcb263fb9c7843b
barb3ra <lar.slugs@gmail.com>
2020-01-23 22:09:19 +0100

    fix old style issues

--- synk-client/src/app/pages/account/login/login.component.ts
+++ synk-client/src/app/pages/account/login/login.component.ts
@@ -6,7 +6,7 @@ import { NzNotificationService } from 'ng-zorro-antd';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
   form: FormGroup;
