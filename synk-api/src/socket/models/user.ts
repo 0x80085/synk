4ed1aba18629dddc7be1a9b723baf041c40d2e0b
barb3ra <lar.slugs@gmail.com>
2020-02-01 23:32:16 +0100

    add create static method to member

--- synk-api/src/socket/models/user.ts
+++ synk-api/src/socket/models/user.ts
@@ -1,3 +1,7 @@
+import * as socketio from 'socket.io';
+
+import { getUsername } from './socket.passport';
+
 export type PermissionLevels = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 
 export enum Roles {
@@ -8,11 +12,20 @@ export enum Roles {
   Newbie = 'Newbie'
 }
 
-export interface RoomMember {
+export class RoomMember {
   id: string;
   userName: string;
   role: Roles;
   permissionLevel: PermissionLevels;
+
+  static create(socket: socketio.Socket): RoomMember {
+    return {
+      id: socket.id,
+      permissionLevel: 1,
+      userName: getUsername(socket),
+      role: Roles.Regular
+    };
+  }
 }
 
 export type RoomMemberDto = RoomMember & { isLeader: boolean };
