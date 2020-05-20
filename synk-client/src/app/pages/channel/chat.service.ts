bd0c602c496a7ec5228215f28a9c96c2dea034a6
lte <lte@bringme.com>
2020-05-20 19:48:20 +0200

    make chat work again

--- synk-client/src/app/pages/channel/chat.service.ts
+++ synk-client/src/app/pages/channel/chat.service.ts
@@ -18,39 +18,20 @@ import { map, withLatestFrom, tap } from 'rxjs/operators';
 export class ChatService {
   messageQueue: Message[] = [];
 
-  // roomMessages$: Observable<Message[]> = new Observable(observer => {
-  //   this.socketService.on('group message', (data: RoomMessage) => {
-  //     this.messageQueue = this.messageQueue.concat(data.content);
-  //     observer.next(this.messageQueue);
-  //   });
-  // });
-  // roomUserConfig$: Observable<RoomUserConfig> = new Observable(observer => {
-  //   this.socketService.on('user config', (data: RoomUserConfig) => {
-  //     this.state.isLoggedInSubject.next(true);
-  //     this.state.isSocketConnectedSub.next(true);
-  //     observer.next(data);
-  //   });
-  // });
-  // roomUserList$: Observable<RoomUserDto[]> = new Observable(observer => {
-  //   this.socket.on('userlist update', (data: RoomUserDto[]) => {
-  //     observer.next(data);
-  //   });
-  // });
-
-  roomMessages$ = this.socketService.listenForEventIfConnected(RoomCommands.GROUP_MESSAGE).pipe(
+  roomMessages$ = this.socketService.listenForEvent(RoomCommands.GROUP_MESSAGE).pipe(
     map((data: RoomMessage) => {
       this.messageQueue = this.messageQueue.concat(data.content);
       return this.messageQueue;
     })
   );
 
-  roomUserConfig$ = this.socketService.listenForEventIfConnected(RoomCommands.USER_CONFIG).pipe(
+  roomUserConfig$ = this.socketService.listenForEvent(RoomCommands.USER_CONFIG).pipe(
     map((data: RoomUserConfig) => {
       return data;
     })
   );
 
-  roomUserList$ = this.socketService.listenForEventIfConnected(RoomCommands.USER_LIST_UPDATE).pipe(
+  roomUserList$ = this.socketService.listenForEvent(RoomCommands.USER_LIST_UPDATE).pipe(
     map((data: RoomUserDto[]) => {
       return data;
     })
@@ -62,37 +43,19 @@ export class ChatService {
   //   this.socketService.sendEvent({ command: RoomCommands.PM, payload: msg });
   // }
 
-  sendGroupMessage(obs: Observable<RoomMessage>) {
-    return this.socketService.emitIfConnected(obs)
-      .subscribe(({ socket, data }) => {
-        const message: RoomMessage = {
-          roomName: data.roomName,
-          content: data.content
-        };
-        socket.emit(RoomCommands.GROUP_MESSAGE, message);
-      });
+  sendMessageToRoom(msg: Message, roomName: string) {
+    const message: RoomMessage = {
+      roomName,
+      content: msg
+    };
+    this.socketService.socket.emit(RoomCommands.GROUP_MESSAGE, message);
   }
 
-  // sendMessageToRoom(msg: Message, roomName: string) {
-  //   const message: RoomMessage = {
-  //     roomName,
-  //     content: msg
-  //   };
-  //   this.socketService.sendEvent({ command: RoomCommands.GROUP_MESSAGE, payload: message });
-  // }
 
-  enterRoom<T>(roomName: string): (src: Observable<T>) => Observable<T> {
-    return (source: Observable<T>) =>
-      source.pipe(
-        tap(() =>console.log('enterroom')),
-        withLatestFrom(this.socketService.socket$),
-        tap(([_, socket]) => {
-          console.log(`command :${RoomCommands.JOIN_ROOM} : roomName: ${roomName}`);
+  enterRoom(roomName: string) {
+    console.log('enterroom');
 
-          socket.emit(RoomCommands.JOIN_ROOM, roomName);
-        }),
-        map(([src]) => src),
-      );
+    this.socketService.socket.emit(RoomCommands.JOIN_ROOM, roomName);
   }
 
   // enterRoom(obs: Observable<string>) {
@@ -106,7 +69,7 @@ export class ChatService {
   // }
 
   giveLeader(obs: Observable<{ member: RoomUserDto, roomName: string }>) {
-    return this.socketService.emitIfConnected(obs)
+    return this.socketService.emit(obs)
       .subscribe(({ socket, data: { member, roomName } }) => {
         const ev = { to: member.userName, roomName };
         socket.emit(RoomCommands.GIVE_LEADER, ev);
@@ -114,18 +77,10 @@ export class ChatService {
 
     // this.socketService.sendEvent({ command: RoomCommands.GIVE_LEADER, payload: ev });
   }
-
-  exit(obs: Observable<string>) {
-    return this.socketService.emitIfConnected(obs)
-      .subscribe(({ socket, data: roomName }) => {
-        socket.emit(RoomCommands.EXIT_ROOM, roomName);
-        socket.off(RoomCommands.GROUP_MESSAGE);
-        this.messageQueue = [];
-
-      });
-    // this.socketService.sendEvent({ command: RoomCommands.EXIT_ROOM, payload: name });
-    // this.socketService.removeEventListener(RoomCommands.GROUP_MESSAGE);
-    // this.messageQueue = [];
+  exit(name: string) {
+    this.socketService.socket.emit(RoomCommands.EXIT_ROOM, name);
+    this.socketService.socket.off(RoomCommands.GROUP_MESSAGE);
+    this.messageQueue = [];
   }
 
   // setupMessageQueueGCTimer() {
