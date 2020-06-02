873c7e5e1203bef19759998047e367b1f1d75b52
lte <lte@bringme.com>
2020-06-02 23:02:37 +0200

    try cytube socketio server settings

--- synk-api/src/socket/setup.ts
+++ synk-api/src/socket/setup.ts
@@ -12,7 +12,35 @@ export function setupSockets(
   sessionMiddleware: SessionOptions,
   logger: Logger
 ) {
-  const io = socketio(wsHttp);
+  const io = socketio(wsHttp, {
+    /*
+     * Set ping timeout to 2 minutes to avoid spurious reconnects
+     * during transient network issues.  The default of 5 minutes
+     * is too aggressive.
+     *
+     * https://github.com/calzoneman/sync/issues/780
+     */
+    pingTimeout: 120000,
+
+    /*
+     * Per `ws` docs: "Note that Node.js has a variety of issues with
+     * high-performance compression, where increased concurrency,
+     * especially on Linux, can lead to catastrophic memory
+     * fragmentation and slow performance."
+     *
+     * CyTube's frames are ordinarily quite small, so there's not much
+     * point in compressing them.
+     */
+    perMessageDeflate: false,
+    httpCompression: false,
+
+    /*
+     * Default is 10MB.
+     * Even 1MiB seems like a generous limit...
+     */
+    // tslint:disable-next-line: no-bitwise
+    maxHttpBufferSize: 1 << 20
+  });
 
   const roomService = new RoomService(io, logger);
 
