e388f565c72dad21a1abdbeb8d97407d6af057df
barb3ra <lar.slugs@gmail.com>
2020-09-24 23:09:32 +0200

    Remove unneeded code

--- synk-client/src/app/pages/channel/media/html5/html5.component.ts
+++ synk-client/src/app/pages/channel/media/html5/html5.component.ts
@@ -3,14 +3,10 @@ import {
   Component,
   ElementRef,
   EventEmitter,
-  Input,
   OnDestroy,
-  Output,
-  Renderer2,
   ViewChild
 } from "@angular/core";
 import { BaseMediaComponent } from '../base-media.component';
-import { EventService } from './events.service';
 
 @Component({
   selector: "app-video",
@@ -19,23 +15,16 @@ import { EventService } from './events.service';
 })
 export class Html5Component implements BaseMediaComponent, AfterViewInit, OnDestroy {
 
-  constructor(private renderer: Renderer2, private evt: EventService) { }
+  constructor() { }
   @ViewChild("video", { static: false }) private video: ElementRef;
 
   src: string;
 
   playing = false;
 
-  isFullscreen = false;
-
-  videoWidth: number;
-  videoHeight: number;
-
   videoLoaded = false;
 
   videoEnded: EventEmitter<unknown> = new EventEmitter();
-
-  private events: any[];
 
   setCurrentUrl(url: string): void {
     this.setVideoSrc(url);
@@ -68,49 +57,26 @@ export class Html5Component implements BaseMediaComponent, AfterViewInit, OnDest
   }
 
   ngAfterViewInit(): void {
-    this.events = [
-      {
-        element: this.video.nativeElement,
-        name: "loadstart",
-        callback: event => (this.videoLoaded = false),
-        dispose: null
-      },
-      {
-        element: this.video.nativeElement,
-        name: "loadedmetadata",
-        callback: event => this.evLoadedMetadata(event),
-        dispose: null
-      },
-      {
-        element: this.video.nativeElement,
-        name: "ended",
-        callback: event => this.videoEnded.next(event),
-        dispose: null
-      },
-      {
-        element: this.video.nativeElement,
-        name: "error",
-        callback: event => console.error("Unhandled Video Error", event),
-        dispose: null
-      },
-
-    ];
+    this.video.nativeElement.onended = () => {
+      this.videoEnded.next();
+    };
+    this.video.nativeElement.onerror = (event: any) => {
+      console.log(event);
+      this.videoEnded.next();
+    };
     this.video.nativeElement.onloadeddata = () => {
       this.videoLoaded = true;
       const player = this.video.nativeElement as HTMLVideoElement;
       player.play();
     };
     this.setVideoSrc(this.src);
-    this.evt.addEvents(this.renderer, this.events);
   }
 
   ngOnDestroy(): void {
     this.video.nativeElement.onloadeddata = null;
-    this.evt.removeEvents(this.events);
   }
 
   load(): void {
-    // Use https://angular.io/api/core/Renderer2
     if (this.video && this.video.nativeElement) {
       this.video.nativeElement.load();
     }
@@ -118,12 +84,6 @@ export class Html5Component implements BaseMediaComponent, AfterViewInit, OnDest
 
   getVideoTag(): HTMLVideoElement | null {
     return this.video && this.video.nativeElement ? (this.video.nativeElement as HTMLVideoElement) : null;
-  }
-
-  evLoadedMetadata(event: any): void {
-    this.videoWidth = this.video.nativeElement.videoWidth;
-    this.videoHeight = this.video.nativeElement.videoHeight;
-    this.videoLoaded = true;
   }
 
   private setVideoSrc(src: string): void {
