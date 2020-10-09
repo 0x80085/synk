e80c0a132ccee999a06833a239f73af977ee011b
barb3ra <lar.slugs@gmail.com>
2020-10-09 15:58:34 +0200

    toggleable logger in client

--- synk-client/src/app/utils/custom.operators.ts
+++ synk-client/src/app/utils/custom.operators.ts
@@ -1,17 +1,18 @@
 import { MonoTypeOperatorFunction } from 'rxjs';
 import { tap } from 'rxjs/operators';
-import { environment } from '../../environments/environment';
 
 export function doLog<T>(msg: string, doLogInput = false): MonoTypeOperatorFunction<T> {
   return input$ => input$.pipe(
     tap((input) => {
-      if (environment.production) {
-        return;
-      }
-      console.log(msg);
-      if (doLogInput) {
-        console.log(input);
-      }
+      debugLog(msg);
+      if (doLogInput) console.log(input);
     })
   );
+}
+
+export function debugLog(msg: string) {
+  if (!(window as any).debug) {
+    return;
+  }
+  console.log(msg);
 }
