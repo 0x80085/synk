58ab20dedcfc74fc20d5ef8e1cea4be79ae6f726
oxbob <larslugs@gmail.com>
2022-11-27 01:55:53 +0100

    cleanup

--- synk-client/src/app/pages/channel/chat-message/chat-message.component.ts
+++ synk-client/src/app/pages/channel/chat-message/chat-message.component.ts
@@ -1,7 +1,12 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { EmojiSearch } from '@ctrl/ngx-emoji-mart';
-import { EmojiService } from '@ctrl/ngx-emoji-mart/ngx-emoji';
-import { BehaviorSubject, map, Observable, of, switchMap, tap } from 'rxjs';
+import {
+  BehaviorSubject,
+  distinctUntilChanged,
+  map,
+  Observable,
+  of,
+  switchMap
+} from 'rxjs';
 import { CUSTOM_EMOJIS } from '../chat-room/message-input/custom-emoji.data';
 import { EmoteService } from '../emote.service';
 import { Message } from '../models/room.models';
@@ -24,37 +29,17 @@ const emoteRegex = /(:[\w,\+,\-]+:)/g;
 export class ChatMessageComponent implements OnChanges {
   customEmojis = CUSTOM_EMOJIS;
 
-  allEmojisByColonKey = [
-    ...this.emojiMart.emojis,
-    ...this.customEmojis.map(({ name, shortNames, imageUrl }) => ({
-      id: name,
-      native: null,
-      shortName: shortNames[0],
-      colons: shortNames[0],
-      name,
-      imageUrl,
-    })),
-  ].reduce((collector, curentItem) => {
-    collector[curentItem.shortName] = curentItem;
-    return collector;
-  }, {});
+  allEmotesById = this.emoteService.allEmojisByColonKey;
 
   @Input() message: Message;
 
-  private userNameSubject = new BehaviorSubject('');
-  userName = this.userNameSubject.pipe();
-
-  private parsedMessageSubject = new BehaviorSubject('');
-  parsedMessage$: Observable<MessagePart[]> = this.parsedMessageSubject.pipe(
-    tap((it) => console.warn('chat message: ', it)),
+  private messageSubject = new BehaviorSubject('');
+  parsedMessage$: Observable<MessagePart[]> = this.messageSubject.pipe(
+    distinctUntilChanged(),
     switchMap((text) =>
       this.emoteService.getEmoteKeysFromText(text).length > 0
         ? of(text).pipe(
-            map((it) => {
-              const splitByEmojiRegex = it.split(emoteRegex);
-              console.warn('chatmessage: ', splitByEmojiRegex);
-              return splitByEmojiRegex;
-            }),
+            map((text) => text.split(emoteRegex)),
             map((parts) => {
               const messageParts: MessagePart[] = parts.map((part) => {
                 if (!emoteRegex.test(part)) {
@@ -63,12 +48,11 @@ export class ChatMessageComponent implements OnChanges {
                     emote: null,
                   };
                 }
-                const withoutColons = part.replaceAll(':', '');
-                console.warn('chat message ', withoutColons);
+                const inputWithoutColons = part.replaceAll(':', '');
                 return {
                   emote: {
-                    imageUrl: this.allEmojisByColonKey[withoutColons]?.imageUrl,
-                    native: this.allEmojisByColonKey[withoutColons]?.native,
+                    imageUrl: this.allEmotesById[inputWithoutColons]?.imageUrl,
+                    native: this.allEmotesById[inputWithoutColons]?.native,
                   },
                   text: part,
                 };
@@ -82,20 +66,14 @@ export class ChatMessageComponent implements OnChanges {
               text,
             } as MessagePart,
           ])
-    ),
-    tap((it) => console.warn('chat message parsed: ', it))
+    )
   );
 
   constructor(
     private emoteService: EmoteService,
-    private emojiMart: EmojiService,
-    private emojiMartSearch: EmojiSearch // wat do
   ) {}
 
   ngOnChanges(changes: SimpleChanges): void {
-    this.parsedMessageSubject.next(
-      (changes['message'] as any).currentValue.text
-    );
-    console.warn('chatmessage: ', changes);
+    this.messageSubject.next((changes['message'] as any).currentValue.text);
   }
 }
