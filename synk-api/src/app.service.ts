1355145798c4355b1681f48b0d45506f630fff63
oxbob <larslugs@gmail.com>
2022-12-05 17:25:45 +0100

    remove dupe code from appservice

--- synk-api/src/app.service.ts
+++ synk-api/src/app.service.ts
@@ -1,42 +1,18 @@
 import { Injectable, Logger } from '@nestjs/common';
 
-import { InjectRepository } from '@nestjs/typeorm';
 import { readFileSync } from 'graceful-fs';
 import { join } from 'path';
 import { cwd } from 'process';
-import { Repository } from 'typeorm';
-import { GlobalSettings, GLOBAL_SETTINGS_NAME } from './domain/entity';
 
 @Injectable()
 export class AppService {
-  globalSettings: GlobalSettings = null;
-
   version: string;
 
   private readonly logger = new Logger(AppService.name);
 
-  constructor(
-    @InjectRepository(GlobalSettings)
-    private readonly settingsRepository: Repository<GlobalSettings>,
-  ) {
+  constructor() {
     this.version = this.readVersionFromPackageJson();
     this.logger.log(`🚀 Starting Synk (version: ${this.version})`);
-
-    this.logger.log(`Loading Global Settings..`);
-    settingsRepository
-      .findOne({ where: { name: GLOBAL_SETTINGS_NAME } })
-      .then((it) => {
-        this.logger.log('Fetched Global settings from DB');
-        if (it === null) {
-          const newSettings = this.settingsRepository.create({ name: GLOBAL_SETTINGS_NAME });
-          this.globalSettings = it;
-          return this.settingsRepository.save(newSettings);
-        }
-        this.globalSettings = it;
-      })
-      .catch((err) =>
-        this.logger.warn('Could not fetch Global Settings from DB...', err),
-      );
   }
 
   private readVersionFromPackageJson() {
